d4204e57b0e44e0b0211ba0794c1307d98b6cfcb
anbochka <an_bochka@inbox.ru>
2016-11-25 18:24:45 +0300

    add chapter_001

---
+++ chapter_001/src/main/java/ru/abochka/Calculate.java
@@ -0,0 +1,12 @@
+package ru.abochka;
+
+/**
+ *Class Calculate решение задачи части 001 урок1.
+ *@author abochka
+ *@since 22.11.16
+ */
+public class Calculate {
+	public static void main(String[] args) {
+		System.out.println( "Hello World." );
+	}
+}
