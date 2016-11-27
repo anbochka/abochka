641caf5f0444a4ae7641bd286dfeee9277bc3e7f
anbochka <an_bochka@inbox.ru>
2016-11-27 18:27:04 +0300

    add test and pacage-info

---
+++ chapter_001/src/test/java/ru/abochka/CalculateTest.java
@@ -0,0 +1,27 @@
+package ru.abochka;
+
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test.
+ *
+ * @author Andrew Bochka (an_bochka@inbox.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class CalculateTest {
+    /**
+     * Test add.
+     */
+    @Test
+    public void whenAddOneToOneThenTwo() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        Calculate.main(null);
+        assertThat(out.toString(), is("Hello World\r\n"));
+    }
+}
---
+++ chapter_001/src/test/java/ru/abochka/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * //TODO add comments.
+ *
+ * @author Andrew Bochka (an_bochka@inbox.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+package ru.abochka;
