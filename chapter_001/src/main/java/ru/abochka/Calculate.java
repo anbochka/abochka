3541fd24e8f1fdc020d68d6adf047ed132797850
anbochka <an_bochka@inbox.ru>
2016-11-27 18:22:09 +0300

    add calculate,java and package-info

--- chapter_001/src/main/java/ru/abochka/Calculate.java
+++ chapter_001/src/main/java/ru/abochka/Calculate.java
@@ -6,7 +6,11 @@ package ru.abochka;
  *@since 22.11.16
  */
 public class Calculate {
+  /**
+  * Main.
+  * @param args - args.
+  */
 	public static void main(String[] args) {
-		System.out.println( "Hello World." );
+		System.out.println("Hello World");
 	}
 }
